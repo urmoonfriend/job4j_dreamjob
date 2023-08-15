8e42c88cbc7cccac43488b05ba4b773ab2bf304b
Aidos <aidos@paygate.kz>
2023-08-15 09:50:42 +0600

    Реализован "Hello World!" с помощью html

--- src/main/java/kz/job4j/dreamjob/controller/IndexController.java
+++ src/main/java/kz/job4j/dreamjob/controller/IndexController.java
@@ -1,12 +1,13 @@
 package kz.job4j.dreamjob.controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
 
     @GetMapping("/index")
     public String getIndex() {
-        return "Hello World!";
+        return "index";
     }
 }
